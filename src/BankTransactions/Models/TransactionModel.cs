982faec64d6f015187999a94ae8c553d4c0cd8cb
TAKENET\gustavo.ferreira <gustavo.ferreira@take.net>
2022-05-02 18:30:44 -0300

    add new model named TransactModel

---
+++ src/BankTransactions/Models/TransactionModel.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BankTransactions.Models
+{
+    public class TransactionModel
+    {
+        [Key]
+        public int TransactId { get; set; }
+
+        public string AccountNumber { get; set; }
+
+        public string BeneficiaryName { get; set; }
+
+        public string BankName { get; set; }    
+
+        public string SwiftCode { get; set; }
+
+        public int Ammount { get; set; }
+
+        public DateTime Date { get; set; }
+    }
+}
