9cbf2ff342d8f4e4ae929a91e38f664cdcd715d2
TAKENET\gustavo.ferreira <gustavo.ferreira@take.net>
2022-05-03 11:40:47 -0300

    format date

--- src/BankTransactions/Models/TransactionModel.cs
+++ src/BankTransactions/Models/TransactionModel.cs
@@ -34,6 +34,7 @@ namespace BankTransactions.Models
         [Required(ErrorMessage = "This field is required.")]
         public int Amount { get; set; }
 
+        [DisplayFormat(DataFormatString = "{0:MMM-dd-yy}")]
         public DateTime Date { get; set; }
     }
 }
