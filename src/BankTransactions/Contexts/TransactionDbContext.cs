2be3c0b5561cf1342d6cbf2934e2993ba0e4e018
TAKENET\gustavo.ferreira <gustavo.ferreira@take.net>
2022-05-03 07:56:19 -0300

    add db context

---
+++ src/BankTransactions/Contexts/TransactionDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using BankTransactions.Models;
+
+namespace BankTransactions.Contexts
+{
+    public class TransactionDbContext : DbContext
+    {
+        public TransactionDbContext(DbContextOptions<TransactionDbContext> options) 
+            : base(options)
+        { }
+
+        public DbSet<TransactionModel> Transactions { get; set; }
+    }
+}
