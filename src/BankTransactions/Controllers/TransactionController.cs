ed78284be8293406e5b0973dd04b70f7fbb6ff6c
TAKENET\gustavo.ferreira <gustavo.ferreira@take.net>
2022-05-03 08:05:59 -0300

    add new controller named TransactionController with MVC views, using Entity Framework

---
+++ src/BankTransactions/Controllers/TransactionController.cs
@@ -0,0 +1,154 @@
+﻿#nullable disable
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using BankTransactions.Contexts;
+using BankTransactions.Models;
+
+namespace BankTransactions.Controllers
+{
+    public class TransactionController : Controller
+    {
+        private readonly TransactionDbContext _context;
+
+        public TransactionController(TransactionDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Transaction
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Transactions.ToListAsync());
+        }
+
+        // GET: Transaction/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var transactionModel = await _context.Transactions
+                .FirstOrDefaultAsync(m => m.TransactId == id);
+            if (transactionModel == null)
+            {
+                return NotFound();
+            }
+
+            return View(transactionModel);
+        }
+
+        // GET: Transaction/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Transaction/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("TransactId,AccountNumber,BeneficiaryName,BankName,SwiftCode,Ammount,Date")] TransactionModel transactionModel)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(transactionModel);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(transactionModel);
+        }
+
+        // GET: Transaction/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var transactionModel = await _context.Transactions.FindAsync(id);
+            if (transactionModel == null)
+            {
+                return NotFound();
+            }
+            return View(transactionModel);
+        }
+
+        // POST: Transaction/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("TransactId,AccountNumber,BeneficiaryName,BankName,SwiftCode,Ammount,Date")] TransactionModel transactionModel)
+        {
+            if (id != transactionModel.TransactId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(transactionModel);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!TransactionModelExists(transactionModel.TransactId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(transactionModel);
+        }
+
+        // GET: Transaction/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var transactionModel = await _context.Transactions
+                .FirstOrDefaultAsync(m => m.TransactId == id);
+            if (transactionModel == null)
+            {
+                return NotFound();
+            }
+
+            return View(transactionModel);
+        }
+
+        // POST: Transaction/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var transactionModel = await _context.Transactions.FindAsync(id);
+            _context.Transactions.Remove(transactionModel);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool TransactionModelExists(int id)
+        {
+            return _context.Transactions.Any(e => e.TransactId == id);
+        }
+    }
+}
