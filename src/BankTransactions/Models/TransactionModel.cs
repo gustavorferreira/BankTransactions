ea5c87547d7453289c67d7e41979d13d96b60f47
TAKENET\gustavo.ferreira <gustavo.ferreira@take.net>
2022-05-03 09:57:56 -0300

    add Display Name and Required on TransactionModel

--- src/BankTransactions/Models/TransactionModel.cs
+++ src/BankTransactions/Models/TransactionModel.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace BankTransactions.Models
@@ -9,17 +10,26 @@ namespace BankTransactions.Models
         public int TransactId { get; set; }
 
         [Column(TypeName ="nvarchar(12)")]
+        [DisplayName("Account Number")]
+        [Required]
         public string AccountNumber { get; set; }
 
         [Column(TypeName = "nvarchar(100)")]
+        [DisplayName("Beneficiary Name")]
+        [Required]
         public string BeneficiaryName { get; set; }
 
         [Column(TypeName = "nvarchar(100)")]
+        [DisplayName("Bank Name")]
+        [Required]
         public string BankName { get; set; }
 
         [Column(TypeName = "nvarchar(11)")]
+        [DisplayName("Swift Code")]
+        [Required]
         public string SwiftCode { get; set; }
 
+        [Required]
         public int Ammount { get; set; }
 
         public DateTime Date { get; set; }
