aa530e827001f36002cebd9e66020f5b80007099
TAKENET\gustavo.ferreira <gustavo.ferreira@take.net>
2022-05-03 10:20:33 -0300

    add message on required

--- src/BankTransactions/Models/TransactionModel.cs
+++ src/BankTransactions/Models/TransactionModel.cs
@@ -11,26 +11,28 @@ namespace BankTransactions.Models
 
         [Column(TypeName ="nvarchar(12)")]
         [DisplayName("Account Number")]
-        [Required]
+        [Required(ErrorMessage = "This field is required.")]
+        [MaxLength(12, ErrorMessage = "Maximum 12 characters only")]
         public string AccountNumber { get; set; }
 
         [Column(TypeName = "nvarchar(100)")]
         [DisplayName("Beneficiary Name")]
-        [Required]
+        [Required(ErrorMessage = "This field is required.")]
         public string BeneficiaryName { get; set; }
 
         [Column(TypeName = "nvarchar(100)")]
         [DisplayName("Bank Name")]
-        [Required]
+        [Required(ErrorMessage = "This field is required.")]
         public string BankName { get; set; }
 
         [Column(TypeName = "nvarchar(11)")]
         [DisplayName("Swift Code")]
-        [Required]
+        [Required(ErrorMessage = "This field is required.")]
+        [MaxLength(11, ErrorMessage = "Maximum 12 characters only")]
         public string SwiftCode { get; set; }
 
-        [Required]
-        public int Ammount { get; set; }
+        [Required(ErrorMessage = "This field is required.")]
+        public int Amount { get; set; }
 
         public DateTime Date { get; set; }
     }
