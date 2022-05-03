2b3f7b4cecb5e4ad7f7eb3614d2b86549f0113da
TAKENET\gustavo.ferreira <gustavo.ferreira@take.net>
2022-05-03 07:50:51 -0300

    add typename of column

--- src/BankTransactions/Models/TransactionModel.cs
+++ src/BankTransactions/Models/TransactionModel.cs
@@ -1,4 +1,5 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace BankTransactions.Models
 {
@@ -7,12 +8,16 @@ namespace BankTransactions.Models
         [Key]
         public int TransactId { get; set; }
 
+        [Column(TypeName ="nvarchar(12)")]
         public string AccountNumber { get; set; }
 
+        [Column(TypeName = "nvarchar(100)")]
         public string BeneficiaryName { get; set; }
 
-        public string BankName { get; set; }    
+        [Column(TypeName = "nvarchar(100)")]
+        public string BankName { get; set; }
 
+        [Column(TypeName = "nvarchar(11)")]
         public string SwiftCode { get; set; }
 
         public int Ammount { get; set; }
